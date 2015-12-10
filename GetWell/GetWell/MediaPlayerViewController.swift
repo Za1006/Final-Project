7b4d4edc96c21447c618ab04ca420e08e673edd9
Elizabeth Elliott <elliott1006@gmail.com>
2015-12-10 12:46:07 -0500

    trying to add audio functions in MediaVC

--- GetWell/GetWell/MediaPlayerViewController.swift
+++ GetWell/GetWell/MediaPlayerViewController.swift
@@ -12,19 +12,36 @@ import AVFoundation
 
 class MediaPlayerViewController: UIViewController
 {
+    @IBOutlet var timeLabel: UILabel!
+    @IBOutlet var songTitleLabel: UILabel!
+    @IBOutlet var artistLabel: UILabel!
+    @IBOutlet var countDownView: UIPickerView!
+    
+    @IBOutlet var playPauseButton: UIButton!
+    
+    let avQueuePlayer = AVQueuePlayer()
+    var songs = Array<Song>()
+    var currentSong: Song?
+    var nowPlaying: Bool = false
 
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        loadCurrentSong()
+        configurePlaylist()
+        setupAudioSession()
 
-        // Do any additional setup after loading the view.
+
+
+
     }
 
     override func didReceiveMemoryWarning()
     {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
+    
     
 
     /*
@@ -36,5 +53,76 @@ class MediaPlayerViewController: UIViewController
         // Pass the selected object to the new view controller.
     }
     */
+    
+   // MARK: - Action Handlers
+    
+    @IBAction func playPauseTapped(sender: UIButton)
+    {
+        togglePlayback(!nowPlaying)
+    }
+    
+    @IBAction func skipForwardTapped(sender: UIButton)
+    {
+        let currentSongIndex = (songs as NSArray).indexOfObject(currentSong!)
+        let nextSong: Int
+        
+        if currentSongIndex + 1 >= songs.count
+        {
+            nextSong = 0
+        }
+        else
+        {
+            nextSong = currentSongIndex + 1
+        }
+        currentSong = songs[nextSong]
+        loadCurrentSong()
+        togglePlayback(true)
+    }
+    
+    @IBAction func skipBackTapped(sender: UIButton)
+    {
+        avQueuePlayer.seekToTime(CMTimeMakeWithSeconds(0.0, 1))
+        if !nowPlaying
+        {
+            togglePlayback(true)
+        }
+    }
+    
+    func configurePlaylist()
+    {
+        
+    }
+    
+    func loadCurrentSong()
+    {
+        avQueuePlayer.removeAllItems()
+        if let song = currentSong
+        {
+            song.playerItem.seekToTime(CMTimeMakeWithSeconds(0.0, 1))
+            avQueuePlayer.insertItem(song.playerItem, afterItem: nil)
+            songTitleLabel.text = song.title
+            artistLabel.text = song.artist
+//          countDownView.image = UIPickerView(time: song.countDownView)
+        }
+    }
+    
+    func setupAudioSession()
+    {
+        
+    }
 
+    func togglePlayback()
+    {
+        nowPlaying = play
+        if play
+        {
+            playPauseButton.setImage(UIImage(named: "Pause"), forState: UIControlState.Normal)
+            avQueuePlayer.play()
+        }
+        else
+        {
+            playPauseButton.setImage(UIImage(named: "Play"), forState: UIControlState.Normal)
+            avQueuePlayer.pause()
+        }
+    }
 }
