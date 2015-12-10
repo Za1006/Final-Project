c065eabd34690e4b78fc9e2edea370444d84e0f4
Elizabeth Elliott <elliott1006@gmail.com>
2015-12-10 14:33:27 -0500

    changed branch name

--- GetWell/GetWell/AppDelegate.swift
+++ GetWell/GetWell/AppDelegate.swift
@@ -10,7 +10,8 @@ import UIKit
 import CoreData
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate
+{
 
     var window: UIWindow?
 
@@ -24,7 +25,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
-    func applicationWillResignActive(application: UIApplication) {
+    func applicationWillResignActive(application: UIApplication)
+    {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
