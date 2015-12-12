eaa802c2a18f78cd1b922bd0d927ad8720467dcc
Michael Reynolds <reynoldsmichael945@gmail.com>
2015-12-11 23:15:03 -0500

    Addition of PopOver with DatePicker

--- GetWell/MainViewController.swift
+++ GetWell/MainViewController.swift
@@ -8,13 +8,19 @@
 
 import UIKit
 
-class MainViewController: UIViewController
+@objc protocol DatePickerDelegate
+{
+    func dateWasChosen(date: NSDate)
+}
+
+class MainViewController: UIViewController, UIPopoverPresentationControllerDelegate, DatePickerDelegate
 {
     
     @IBOutlet weak var image: UIImage!
     @IBOutlet weak var textView: UITextView!
     @IBOutlet weak var TimeSegmentedControl: UISegmentedControl!
     
+    var remainingCharacters = ["Obi-Wan Kenobi", "Leia Organa", "R2-D2", "Luke Skywalker", "Grand Moff Tarkin", "Darth Vader"]
 
     override func viewDidLoad()
     {
@@ -29,21 +35,49 @@ class MainViewController: UIViewController
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
     
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?)
+    {
+        if segue.identifier == "SetReminderSegue"
+        {
+            let destVC = segue.destinationViewController as! SetReminderPopOverViewController
+                destVC.popoverPresentationController?.delegate = self
+            let contentHeight = 50.0 * CGFloat(remainingCharacters.count)
+                destVC.preferredContentSize = CGSizeMake(400.0, contentHeight)
+        }
+    }
+    
+    // MARK: - UIPopoverPresentationController Delegate
+    
+    func adaptivePresentationStyleForPresentationController(controller: UIPresentationController) -> UIModalPresentationStyle
+    {
+        return UIModalPresentationStyle.None
+        //can also just type return .None
+    }
+    
+    // MARK: DatePicker Delegate
+    
+    func dateWasChosen(date: NSDate)
+    {
+//        destinationTime.text = dateFormat(date)
+    }
+    
+    func dateFormat(x: NSDate) -> String
+    {
+        let formatter = NSDateFormatter()
+        formatter.dateFormat = NSDateFormatter.dateFormatFromTemplate("MMM dd yyyy", options: 0, locale: NSLocale.currentLocale())
+        let formattedTime = formatter.stringFromDate(x).uppercaseString
+        
+        return String(formattedTime)
+    }
+
     func setNotificationTapped()
     {
         
     }
+    
     @IBAction func changeSortCriteria(sender: UISegmentedControl)
     {
         
