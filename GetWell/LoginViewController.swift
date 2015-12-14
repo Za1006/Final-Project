09a54c7f761b925517ed6c26679f94fa7e355e5b
Keron Williams <keronwilliams9@gmail.com>
2015-12-14 10:45:13 -0500

    + trial

--- GetWell/LoginViewController.swift
+++ GetWell/LoginViewController.swift
@@ -14,6 +14,7 @@ class LoginViewController: UIViewController
     var liz: String?
     var mike: String?
     var keron: String?
+    var stupidXcode: String?
     
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
