4af24def2be366c8dfda73a395c995f1245992a0
Michael Reynolds <reynoldsmichael945@gmail.com>
2015-12-14 11:06:35 -0500

    test dad

--- GetWell/LoginViewController.swift
+++ GetWell/LoginViewController.swift
@@ -11,14 +11,12 @@ import UIKit
 class LoginViewController: UIViewController
 {
     
-<<<<<<< HEAD
     var peoples = 3
-=======
     var liz: String?
     var mike: String?
     var keron: String?
     var stupid: String?
->>>>>>> master
+    var dad = 7
     
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
