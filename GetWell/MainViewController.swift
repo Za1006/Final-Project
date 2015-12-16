6cdedc3d82ed3d82b83d7624901d2b334376a455
Keron Williams <keronwilliams9@gmail.com>
2015-12-16 14:08:13 -0500

    + NewChanges

--- GetWell/MainViewController.swift
+++ GetWell/MainViewController.swift
@@ -7,23 +7,20 @@
 //
 
 import UIKit
+import CoreData
 
 @objc protocol DatePickerDelegate
 {
     func dateWasChosen(date: NSDate)
 }
 
-@objc protocol StepsListViewDelegate
-{
-    func stepsChecked(buttonTapped: Int)
-}
 @objc protocol MediaPlayerViewDelegate
 {
     func timerWasChosen(timerCount: Int)
 }
 
 
-class MainViewController: UIViewController, UIPopoverPresentationControllerDelegate, DatePickerDelegate, StepsListViewDelegate,MediaPlayerViewDelegate
+class MainViewController: UIViewController, UIPopoverPresentationControllerDelegate, DatePickerDelegate, MediaPlayerViewDelegate, UITableViewDataSource, UITableViewDelegate
 {
     
     
@@ -35,18 +32,36 @@ class MainViewController: UIViewController, UIPopoverPresentationControllerDeleg
     @IBOutlet weak var nextMeditation: UILabel!
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var image: UIImage!
-    @IBOutlet weak var TimeSegmentedControl: UISegmentedControl!
+    //@IBOutlet weak var TimeSegmentedControl: UISegmentedControl!
     
     var remainingCharacters = ["Obi-Wan Kenobi", "Leia Organa", "R2-D2", "Luke Skywalker", "Grand Moff Tarkin", "Darth Vader"]
+    
+    var stepsArray = Array<Steps>()
+    let managedObjectContext = (UIApplication.sharedApplication().delegate as! AppDelegate).managedObjectContext
+    
+    let checkImg = UIImage(named: "checked.png")
+    let uncheckImg = UIImage(named: "unchecked.png")
+    var buttonIndex: NSIndexPath!
 
     override func viewDidLoad()
     {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        title = "To do"
+        
+        let fetchRequest = NSFetchRequest(entityName: "Steps")
+        do {
+            let fetchResults = try managedObjectContext.executeFetchRequest(fetchRequest) as? [Steps]
+            stepsArray = fetchResults!
+        }
+        catch {
+            let nserror = error as NSError
+            NSLog("Unresolved error \(nserror), \(nserror.userInfo)")
+            abort()
+
     }
 
-    override func didReceiveMemoryWarning()
+    func didReceiveMemoryWarning()
     {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -54,7 +69,7 @@ class MainViewController: UIViewController, UIPopoverPresentationControllerDeleg
     
     // MARK: - Navigation
     
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?)
+    func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?)
     {
         if segue.identifier == "ShowCountdownSegue"
         {
@@ -115,43 +130,172 @@ class MainViewController: UIViewController, UIPopoverPresentationControllerDeleg
         return String(formattedTime)
     }
     
-    func stepsChecked(buttonTapped: Int)
-    {
+    // MARK: Steps_Check_List (TableView)
         
-    }
+        func numberOfSectionsInTableView(tableView: UITableView) -> Int
+        {
+            return 1
+        }
+        
+        func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+        {
+            return stepsArray.count
+        }
+        
+        override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+        {
+            let cell = tableView.dequeueReusableCellWithIdentifier("StepsListCell", forIndexPath: indexPath) as! StepListCell
+            
+            let todoItem = stepsArray[indexPath.row]
+            
+            if todoItem.title == nil
+            {
+                cell.checkBox.setImage(uncheckImg, forState: UIControlState.Normal)
+                cell.stepsLabel.text = "Breath"
+            }
+            else
+            {
+                cell.stepsLabel.text = todoItem.title
+            }
+            
+            if todoItem.isDone
+            {
+                cell.checkBox.setImage(checkImg, forState: UIControlState.Normal)
+                cell.backgroundColor = UIColor(red: 0.28, green: 0.20, blue: 0.52, alpha: 1)
+                cell.stepsLabel.textColor = UIColor.whiteColor()
+            }
+            else
+            {
+                cell.checkBox.setImage(uncheckImg, forState: UIControlState.Normal)
+                cell.backgroundColor = UIColor(red:0.91, green:0.91, blue:0.91, alpha:1.0)
+                cell.stepsLabel.textColor = UIColor.greenColor()
+            }
+            
+            return cell
+        }
+
+        
+        // Override to support conditional editing of the table view.
+        override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool
+        {
+            // Return false if you do not want the specified item to be editable.
+            return false
+        }
+
+        // Override to support editing the table view.
+        override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath)
+        {
+            let todoItem = stepsArray[indexPath.row]
+            
+            if todoItem.isDone
+            {
+                let todoItem = stepsArray[indexPath.row]
+                
+                if editingStyle == .Delete
+                {
+                    stepsArray.removeAtIndex(indexPath.row)
+                    managedObjectContext.deleteObject(todoItem)
+                }
+                
+                saveSlot1()
+                
+                tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+            }
+//            else
+//            {
+//                errorLabel.text = "Check tasks before you delete them!"
+//            }
+        }
+
+
     
 
     
-    @IBAction func changeSortCriteria(sender: UISegmentedControl)
-    {
-        
-        TimeSegmentedControl.addTarget(self, action: "action:", forControlEvents: .TouchUpInside)
-
-        if sender.selectedSegmentIndex == 0
-        {
-            
-        }
-        else if sender.selectedSegmentIndex == 1
-        {
-            
-        }
-        else if sender.selectedSegmentIndex == 2
-        {
-            
-        }
-        else if sender.selectedSegmentIndex == 3
-        {
-            
-        }
-        else
-        {
-            
-        }
-        
-    }
+//    @IBAction func changeSortCriteria(sender: UISegmentedControl)
+//    {
+//        
+//        TimeSegmentedControl.addTarget(self, action: "action:", forControlEvents: .TouchUpInside)
+//
+//        if sender.selectedSegmentIndex == 0
+//        {
+//            
+//        }
+//        else if sender.selectedSegmentIndex == 1
+//        {
+//            
+//        }
+//        else if sender.selectedSegmentIndex == 2
+//        {
+//            
+//        }
+//        else if sender.selectedSegmentIndex == 3
+//        {
+//            
+//        }
+//        else
+//        {
+//            
+//        }
+//        
+//    }
     
     // MARK: Steps_Check_List (TableView)
     
+
+        //MARK: - ACTION HANDLERS 3: REVENGE OF THE HANDLERS
+        @IBAction func addTodo(sender: UIButton)
+        {
+            let todoItem = NSEntityDescription.insertNewObjectForEntityForName("Steps", inManagedObjectContext: managedObjectContext) as! Steps
+            todoArray.append(todoItem)
+            tableView.reloadData()
+        }
+        
+        @IBAction func checkboxPressed(sender: UIButton)
+        {
+            let contentView = sender.superview
+            let cell = contentView?.superview as! StepListCell
+            let indexPath = tableView.indexPathForCell(cell)
+            let todoItem = stepsArray[indexPath!.row]
+            
+            todoItem.title = cell.titleTextField.text //just in case they don't press enter when they're typing in their todo and just immediately check it off
+            
+            if sender.currentImage == uncheckImg
+            {
+                cell.checkBox.setImage(checkImg, forState: UIControlState.Normal)
+                todoItem.isDone = true
+                //            cell.backgroundColor = UIColor.lightTextColor()
+                cell.backgroundColor = UIColor(red:0.38, green:0.00, blue:0.02, alpha:1.0)
+                cell.stepsLabel.textColor = UIColor.whiteColor()
+            }
+            else
+            {
+                cell.checkBox.setImage(uncheckImg, forState: UIControlState.Normal)
+                todoItem.isDone = false
+                cell.backgroundColor = UIColor(red:0.91, green:0.91, blue:0.91, alpha:1.0)
+                cell.stepsLabel.textColor = UIColor.greenColor()
+            }
+            
+            tableView.reloadData()
+            
+            saveSlot1()
+        }
+        
+        func saveSlot1()
+        {
+            do
+            {
+                try managedObjectContext.save()
+                //save all managed objects
+            }
+            catch
+            {
+                let nserror = error as NSError
+                NSLog("WHOOPS SOMETHING WENT WRONG AHAHA \(nserror), \(nserror.userInfo)")
+                abort() //<<<<<
+            }
+        }
+    }
+
     
     
 }
---
+++ GetWell/Steps.swift
@@ -0,0 +1,14 @@
+//
+//  Steps.swift
+//  GetWell
+//
+//  Created by Keron Williams on 12/16/15.
+//  Copyright © 2015 The Iron Yard. All rights reserved.
+//
+
+import Foundation
+
+class Steps
+{
+    <#properties and methods#>
+}
