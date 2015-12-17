e7eed2b85f8ddc248b3e3fba903fb76904cca0e2
Michael Reynolds <reynoldsmichael945@gmail.com>
2015-12-16 19:41:54 -0500

    ?

--- GetWell/MainViewController.swift
+++ GetWell/MainViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+
 @objc protocol DatePickerDelegate
 {
     func dateWasChosen(date: NSDate)
@@ -17,46 +18,41 @@ import UIKit
 {
     func stepsChecked(buttonTapped: Int)
 }
-@objc protocol MediaPlayerViewDelegate
-{
-    func timerWasChosen(timerCount: Int)
-}
 
 
-class MainViewController: UIViewController, UIPopoverPresentationControllerDelegate, DatePickerDelegate, StepsListViewDelegate,MediaPlayerViewDelegate
+class MainViewController: UIViewController, UIPopoverPresentationControllerDelegate, DatePickerDelegate, StepsListViewDelegate
 {
     
     
-    var delegate: MediaPlayerViewController?
-    var originalCount = 120
     var timer: NSTimer?
-    
     
     @IBOutlet weak var nextMeditation: UILabel!
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var image: UIImage!
-    @IBOutlet weak var TimeSegmentedControl: UISegmentedControl!
+    @IBOutlet var skipSessionLabel: UILabel!
+
     
-    var remainingCharacters = ["Obi-Wan Kenobi", "Leia Organa", "R2-D2", "Luke Skywalker", "Grand Moff Tarkin", "Darth Vader"]
+    var remainingCharacters = []
 
     override func viewDidLoad()
     {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning()
     {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
+    
+    
+    
     
     // MARK: - Navigation
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?)
     {
-        if segue.identifier == "ShowCountdownSegue"
+        if segue.identifier == "ShowMediaSegue"
         {
             let mediaPlayerVC = segue.destinationViewController as! MediaPlayerViewController
             mediaPlayerVC.delegate = self
@@ -79,11 +75,6 @@ class MainViewController: UIViewController, UIPopoverPresentationControllerDeleg
         //can also just type return .None
     }
     
-//    Timer chosen from Segmented (5, 10, 15, 20)
-    func timerWasChosen(timerCount: Int)
-    {
-        originalCount = timerCount
-    }
     
     // MARK: DatePicker Delegate
     
@@ -103,9 +94,6 @@ class MainViewController: UIViewController, UIPopoverPresentationControllerDeleg
         UIApplication.sharedApplication().scheduleLocalNotification(localNotification)
     }
 
-
-   
-    
     func dateFormat(x: NSDate) -> String
     {
         let formatter = NSDateFormatter()
@@ -115,13 +103,12 @@ class MainViewController: UIViewController, UIPopoverPresentationControllerDeleg
         return String(formattedTime)
     }
     
+    
     func stepsChecked(buttonTapped: Int)
     {
         
     }
-    
 
-    
     @IBAction func changeSortCriteria(sender: UISegmentedControl)
     {
         
@@ -150,8 +137,11 @@ class MainViewController: UIViewController, UIPopoverPresentationControllerDeleg
         
     }
     
+=======
+  
+>>>>>>> origin/master
     // MARK: Steps_Check_List (TableView)
     
-    
-    
 }
+
+
