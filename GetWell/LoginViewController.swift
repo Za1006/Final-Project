1bce5f6926bb13cdb3b7caa2a0a0984811e66522
Elizabeth Elliott <elliott1006@gmail.com>
2015-12-20 13:21:12 -0500

    ..

--- GetWell/LoginViewController.swift
+++ GetWell/LoginViewController.swift
@@ -53,7 +53,7 @@ class LoginViewController: UIViewController
                 if user != nil
                 {
                     print("login successful")
-                    self.performSegueWithIdentifier("GetWellViewController", sender: self)
+                    self.performSegueWithIdentifier("MainViewController", sender: self)
                         self.dismissViewControllerAnimated(true, completion: nil)
                 }
                 else
